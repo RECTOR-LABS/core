478b434c2c512bc207db7e6c300fcc3bc8a88575
RECTOR <rector@rectorspace.com>
2026-05-30 14:02:37 +0700

    feat: add journal index and show routes, controller, and views

---
+++ app/controllers/journal_controller.rb
@@ -0,0 +1,16 @@
+class JournalController < ApplicationController
+  def index
+    @posts = Post.recent
+  end
+
+  def show
+    @post = Post.find(params[:slug])
+    return render_not_found if @post.nil? || @post.draft?
+  end
+
+  private
+
+  def render_not_found
+    render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
+  end
+end
